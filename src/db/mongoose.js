b860c4fdd9e6b5a3d4388eab9b941b2699cabe72
Arindam Halder <halderarindam10000@gmail.com>
2022-08-22 13:44:22 +0530

    final commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://localhost:27017/wisemeal-api", {useNewUrlParser:true, useUnifiedTopology:true, autoIndex:true});
+
+// mongoose.connect(`mongodb+srv://wisemeal369:${process.env.MONGOOSE_PASS}@cluster0.ktkyasz.mongodb.net/wisemeal-api?retryWrites=true&w=majority`, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+// })
+mongoose.connect(`mongodb+srv://Nutrifit123:${process.env.MONGOOSE_PASS}@cluster0.urccrhz.mongodb.net/nutrifit-api?retryWrites=true&w=majority`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
